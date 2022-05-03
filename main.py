bcd238831ff721138ce039b8553e5eef892ad3e0
Cole Sims <sims@vt.edu>
2022-05-02 21:58:13 -0400

    added ensemble model and price predict output

--- main.py
+++ main.py
@@ -308,7 +308,7 @@ print('Ensemble MSE: ', full_mse)
 
 output_data['direc'] = clf.predict(output_data)
 full_predict = regr2.predict(output_data)
-d = {'pred':full_predict,'actual':targets['Target_Val']}
+d = {'current':output_data['TSLA Close/Last'],'pred':full_predict,'actual':targets['Target_Val']}
 df = pd.DataFrame(data=d,index=output_data.index)
 df.to_csv('price_predict.csv')
 
